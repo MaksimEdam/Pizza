7abf94a9a3e714fbd9b0969b55307ffb7163c6d3
MaksimEdam <Edamenko6@gmail.com>
2022-06-06 14:04:35 +0300

    section Cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,10 @@ export default function Home() {
         setItems(array);
         setIsLoading(false);
       });
+    window.scroll(0, 0);
   }, []);
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -28,6 +29,6 @@ export default function Home() {
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 }
